f6e8c031768b88dc32142e8774d7c5eb888217f7
Akanksh Rai <akankshraip@gmail.com>
2025-04-09 00:29:54 +0530

    gemini chatbot done

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
+        textTheme: const TextTheme(
+          bodySmall: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+          titleMedium: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue, 
+            foregroundColor: Colors.white, 
+          ),
+        ),
       ),
       home: const HomePage(),
     );
   }
 }
-
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,36 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: Text(
+          "AutiCare Connect",
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(fontSize: 22, color: Colors.white),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+          ),
+          BottomNavigationBarItem(
+        icon: Icon(Icons.medical_services),
+        label: 'Doctors',
+          ),
+          BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          // Handle navigation logic here
+        },
+      ),
       body: Center(
         child: Text("Auticare"),
       ),
